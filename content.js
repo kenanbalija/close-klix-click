120fa487991255d82e3b5262c05b97ba48d4f6c4
Kenan <kenanbalija.dev@gmail.com>
2023-10-15 20:29:46 +0200

    close youtube

--- content.js
+++ content.js
@@ -1,16 +1,21 @@
 const loc = window.location.toString();
 const isKlix = loc.indexOf('klix.ba') !== -1;
 const isAvaz = loc.indexOf('avaz.ba') !== -1;
+const isYoutube = loc.indexOf('youtube.com') !== -1;
 
-if (isKlix || isAvaz) {
+
+if (isKlix || isAvaz || isYoutube) {
     let activated = false;
     let seconds = 0;
     const closeBtnInt = setInterval(() => {
         seconds += 0.1
-        if (document.getElementsByClassName('fc-close')[0] !== undefined) {
+        if (isYoutube && document.getElementsByClassName('yt-spec-button-view-model')[0] !== undefined) {
+            eventFire(document.getElementsByClassName('yt-spec-button-view-model')[0], 'click')
+        } else if (!isYoutube && document.getElementsByClassName('fc-close')[0] !== undefined) {
             eventFire(document.getElementsByClassName('fc-close')[0], 'click')
             activated = true
         }
+        
         if (activated || seconds > 5) {
             clearInterval(closeBtnInt)
         }
